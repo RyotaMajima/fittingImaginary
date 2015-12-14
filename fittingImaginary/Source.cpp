a80b21f8cae6113d0a684eff6f157e7a42e322f8
Ryota Majima <ryota.0571@gmail.com>
2015-12-14 13:28:55 +0900

    [modify] potential depth (shallow)

--- fittingImaginary/Source.cpp
+++ fittingImaginary/Source.cpp
@@ -32,14 +32,14 @@ const double L = X_END - X_BEGIN; //空間幅
 const int N = 512; //空間分割数
 const double DELTA_X = L / N;
 
-const double T_END = 400; //終了時刻
+const double T_END = 50; //終了時刻
 const int TN = T_END * 100; //時間分割数
 const double dt = T_END / TN; //時間刻み幅
 
 const double S = 0.9; //波束の幅
 const double X = -0.5; //初期波束の原点からのずれ
 const double X_OPT = 4.0; //光学ポテンシャルをかける位置
-const double b = 1.0 / 3.0; //3次ポテンシャルの係数
+const double b = 1.0 / 2.5; //3次ポテンシャルの係数
 
 const double E_BEGIN_real = -1.2, E_END_real = 0.0;
 const int EN_real = 200;
--- fittingImaginary/fit_result.plt
+++ fittingImaginary/fit_result.plt
@@ -2,6 +2,7 @@ unset multiplot; reset
 
 set multiplot layout 2,1
 set xran [0:500]
+set grid
 set xlab "T"; set ylab "E_{0}^{I}"
 set format y "%.2e"
 
